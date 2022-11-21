cdc720a7a5b072dd3d488e4d2923f9370c24c803
KarenDBPaz <113690639+KarenDBPaz@users.noreply.github.com>
2022-11-21 11:10:32 -0300

    Correção dos Arquivos
    Signed-off-by: KarenDBPaz <113690639+KarenDBPaz@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const express= require('express');
-const app= express();
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+  "/todos_os_servicos": "/servicos",
+  "/adicionar_servico": "/servicos",
+  "/editar_servico/:id": "/servicos/:id",
+  "/deletar_servico/:id": "/servicos/:id"
+  
+}))
 server.use(router);
-server.listen(port, () => {
-    console.log(`JSON Server está funcionando na porta ${port}`);
-});
+server.listen(port, () => { console.log(`JSON Server está rodando na porta ${port}`) })
